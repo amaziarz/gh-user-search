9776c5a0355db83360ea9fca663b237ec3d3d82d
Adam Maziarz <amaziarz.dev@gmail.com>
2025-05-09 15:52:54 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import prettier from 'eslint-config-prettier/flat';
+import perfectionist from 'eslint-plugin-perfectionist';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import { join } from 'node:path';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  perfectionist.configs['recommended-natural'],
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  pluginQuery.configs['flat/recommended'],
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.js', 'vite.config.ts'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  includeIgnoreFile(join(import.meta.dirname, '.gitignore')),
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    },
+  },
+);
---
+++ src/Providers.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+type Props = {
+  children: ReactNode;
+};
+
+export function Providers({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
